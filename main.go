3f9b247e58ea317203b1ea258727165cb9345e2e
chris.han <dave.han@nsuslab.com>
2023-08-28 21:07:52 +0900

    Create table if not exists

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"cloud.google.com/go/bigquery"
 	"context"
 	"errors"
-	"fmt"
-	"google.golang.org/api/iterator"
+	"google.golang.org/api/googleapi"
 	"google.golang.org/api/option"
 	"log"
 	"math/big"
+	"net/http"
 	"time"
 )
 
@@ -37,43 +37,74 @@ func main() {
 	}
 	defer client.Close()
 
-	table := client.Dataset(datasetID).Table(tableID)
-	inserter := table.Inserter()
+	//dataset := client.Dataset(datasetID)
+	//err = dataset.Table(tableID).Create(ctx, &bigquery.TableMetadata{
+	//	ExpirationTime: time.Now().Add(1 * time.Hour),
+	//})
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	tableRef := client.Dataset(datasetID).Table(tableID)
+	if _, err = tableRef.Metadata(ctx); err != nil {
+		var e *googleapi.Error
+		if errors.As(err, &e) {
+			if e.Code == http.StatusNotFound {
+				if err = tableRef.Create(ctx, &bigquery.TableMetadata{
+					Name: "deposit",
+					Schema: bigquery.Schema{
+						{Name: "Date", Required: true, Type: bigquery.TimestampFieldType},
+						{Name: "Brand", Required: true, Type: bigquery.StringFieldType},
+						{Name: "Country", Required: true, Type: bigquery.StringFieldType},
+						{Name: "PspName", Required: true, Type: bigquery.StringFieldType},
+						{Name: "PaymentMethod", Required: true, Type: bigquery.StringFieldType},
+						{Name: "Amount", Required: true, Type: bigquery.BigNumericFieldType},
+						{Name: "Currency", Required: true, Type: bigquery.StringFieldType},
+						{Name: "Status", Required: true, Type: bigquery.StringFieldType},
+					},
+				}); err != nil {
+					log.Fatalf("create bigquery table failed")
+				}
+			}
+		}
+	}
+
+	inserter := tableRef.Inserter()
 
 	items := []*Deposit{
 		{
 			Date:          time.Now(),
 			Brand:         "GGPCOM",
-			Country:       "KR",
+			Country:       "CH",
 			PspName:       "VISA",
 			PaymentMethod: "CreditCard",
-			Amount:        new(big.Rat).SetFloat64(19800.0098),
+			Amount:        new(big.Rat).SetFloat64(98980.00),
 			Currency:      "USD",
 			Status:        "Init",
 		},
 	}
 
-	if err = inserter.Put(ctx, items); err != nil {
-		log.Fatalf("Failed to insert items: %v", err)
-	}
-
-	fmt.Println("Items inserted successfully!")
-
-	query := client.Query("SELECT pspName, amount FROM `big-query-with-go.metric.deposit`")
-	it, err := query.Read(ctx)
-	if err != nil {
-		log.Fatalf("Failed to initiate read: %v", err)
-	}
-
-	for {
-		var item Deposit
-		err = it.Next(&item)
-		if errors.Is(err, iterator.Done) {
-			break
+	for i := 0; i < 100; i++ {
+		if err = inserter.Put(ctx, items); err != nil {
+			log.Fatalf("Failed to insert items: %v", err)
 		}
-		if err != nil {
-			log.Fatalf("Failed to read data: %v", err)
-		}
-		fmt.Printf("Name: %s, Value: %v\n", item.PspName, item.Amount)
+		log.Println("insert item ", i)
 	}
+
+	//query := client.Query("SELECT pspName, amount FROM `big-query-with-go.metric.deposit`")
+	//it, err := query.Read(ctx)
+	//if err != nil {
+	//	log.Fatalf("Failed to initiate read: %v", err)
+	//}
+	//
+	//for {
+	//	var item Deposit
+	//	err = it.Next(&item)
+	//	if errors.Is(err, iterator.Done) {
+	//		break
+	//	}
+	//	if err != nil {
+	//		log.Fatalf("Failed to read data: %v", err)
+	//	}
+	//	fmt.Printf("Name: %s, Value: %v\n", item.PspName, item.Amount)
+	//}
 }
